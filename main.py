bfaf24dfdacba25b155e295c69c7de821b43f7cd
cursed_noctis <noctisinmementos@gmail.com>
2024-12-28 21:30:18 +0200

    Before adding sorting

--- main.py
+++ main.py
@@ -2,8 +2,26 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = wordcount(text)
-    print(f"wordcount of document:{num_words}")
-    #print(text)
+    characters = characters_list(text)
+    print("--Beginning report of frankenstein.txt--")
+    print(f"{num_words} words found in the document")
+    print(f"Characters calculated: {characters_list(text)}")
+    print("--End of report--")
+
+
+
+def characters_list(text):
+    num_characters = {}
+    lowercased_book = text.lower()
+    for character in lowercased_book:
+        if character not in num_characters:
+            num_characters[character] = 1
+        else:
+            num_characters[character] +=1
+    return num_characters
+
+
+
 
 def wordcount(text):
     words = text.split()
